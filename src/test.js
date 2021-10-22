fdb4f8e519b03b10481e147c31d3e5643464aaa0
Dinh Nho Bao <nhobao2000@gmail.com>
2021-10-23 03:53:59 +0800

    Add input fields

--- src/test.js
+++ src/test.js
@@ -20,21 +20,58 @@ export default class Test extends React.Component {
         super(props);
         this.state = {
             items: items,
+            inputs: {
+                0: {
+                    type: 0,
+                    input1: "x",
+                    input2: "1",
+                },
+                1: {
+                    type: 0,
+                    input1: "x",
+                    input2: "1",
+                },
+                2: {
+                    type: 0,
+                    input1: "x",
+                    input2: "1",
+                },
+                3: {
+                    type: 0,
+                    input1: "x",
+                    input2: "1",
+                }
+            }, // array of tokens
             i: 5
         };
     }
 
+    handleChange(event, id, input) {
+        const newInputs = JSON.parse(JSON.stringify(this.state.inputs));
+        newInputs[id][input] = event.target.value;
+        this.setState({ inputs: newInputs });
+
+        console.log(newInputs);
+    }
+
     onClickFunc = () => {
         const newItems = [...this.state.items];
+        const newInputs = JSON.parse(JSON.stringify(this.state.inputs));
         newItems.push({ id: this.state.i, text: `index ${this.state.i}` });
-        console.log(this.state.i);
+        newInputs[this.state.i] = {
+            type: 0,
+            input1: "x",
+            input2: "1",
+        }
 
-        this.setState({ items: newItems, i: this.state.i + 1 });
+        this.setState({ items: newItems, i: this.state.i + 1, inputs: newInputs });
         console.log(this.state);
+
+        console.log(newInputs);
     }
 
     onDragEnd = (items, item, path) => {
-        console.log(`${item} ${path}`);
+        // console.log(`${item} ${path}`);
         console.log(items);
         const newItems = JSON.parse(JSON.stringify(items));
         this.setState(newItems);
@@ -44,7 +81,33 @@ export default class Test extends React.Component {
     renderItem = ({ item }) =>
         <div tabIndex="0" onKeyDown={(e) => this.handleDelete(e, item)}>
             {item.text}
+
+            {this.getInputField(item.id)}
         </div >;
+
+    getInputField = (id) => {
+        let fields = this.state.inputs[id];
+        /*
+        // json of {
+            type:
+            input1:
+            input2:
+        }
+        */
+
+        /*
+        operator, operand, operand
+        */
+        switch (fields["type"]) {
+            case 0: // +=
+                return <div>
+                    <input type="text" value={this.state.inputs[id]["input1"]} onChange={(e) => this.handleChange(e, id, "input1")}></input>
+                    +=
+                    <input type="text" value={this.state.inputs[id]["input2"]} onChange={(e) => this.handleChange(e, id, "input2")}></input>
+                </div>
+                break;
+        }
+    }
 
     handleDelete = (e, item) => {
         const DELETE = "46";
